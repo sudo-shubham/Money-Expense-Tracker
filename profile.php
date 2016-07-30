f147fafbd499fcc57c49c4bec2df579955c9d30b
Shubh <shubhampatelsp812@gmail.com>
2016-07-30 18:22:36 +0530

    - Added Profile Page to View Current Expenses (Front End Only) - Optimized CSS

--- profile.php
+++ profile.php
@@ -1,4 +1,51 @@
 <?php
 session_start();
-echo "Welcome ".$_SESSION['name'];
 ?>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Money Expense Tracker</title>
+    <link rel="stylesheet" type="text/css" href="css/normalize.css"/>
+    <link rel="stylesheet" type="text/css" href="css/demo.css"/>
+    <link rel="stylesheet" type="text/css" href="css/login.css"/>
+</head>
+<body class="content bgcolor-4">
+<div class="container">
+    <h3 style="color:#FFF; float:left; margin-left:20px;">Welcome, <?php echo $_SESSION['name']; ?></h3>
+    <h3 style="color:#FFF; float:right; margin-right:20px;"><?php echo "Current Salary : Rs. " . $_SESSION['salary']; ?></h3>
+    <h2 style="color:#FFF; margin-top:100px;">Current Expenses</h2>
+    <table id="issues">
+        <tr>
+            <th>Category</th>
+            <th>Description</th>
+            <th>Date</th>
+            <th>Amount</th
+        </tr>
+        <tr>
+            <form method="post" action="expense.php">
+                <td><select name="category">
+                        <option value="Personal">Personal</option>
+                        <option value="Utility Bills">Utility Bills</option>
+                        <option value="Food">Food</option>
+                        <option value="Transportation">Transportation</option>
+                    </select></td>
+                <td><input type="text" name=""desc" placeholder="Description"></td>
+
+                <td><input type="date" name="date" placeholder="DD/MM/YYYY Format"></td>
+                <td><input type="number" placeholder="Rs Spent"><input type="submit" value="Add Expense"></td>
+            </form>
+        </tr>
+        <tr>
+            <th colspan="4">Total Spent:-</th>
+        </tr>
+        <tr>
+            <th colspan="4">Savings:-</th>
+        </tr>
+    </table>
+
+</div>
+</body>
+</html>
